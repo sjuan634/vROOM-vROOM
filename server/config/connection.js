b3528129cc8804c0c2ceba956439042977359420
sanchezju <sjuan634@yahoo.com>
2023-04-02 08:25:17 -0700

    try and fix atlas seed issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/vroom-vroom', {
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useNewUrlParser: true
 });
 
 module.exports = mongoose.connection;
