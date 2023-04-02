1c73a4c27cae15bf338ab38f6a32bb8e4a68b63e
sanchezju <sjuan634@yahoo.com>
2023-04-02 09:26:13 -0700

    revert connection code

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/vroom-vroom', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://m001-student:Ibtisam@sandbox.xuwkkn8.mongodb.net/Property?retryWrites=true&w=majority', {
     useUnifiedTopology: true,
     useFindAndModify: false,
     useNewUrlParser: true
